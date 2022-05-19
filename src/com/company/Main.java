2356a44fba68f9a32f2692bced226ea0a0743b99
Celiek <63073744+Celiek@users.noreply.github.com>
2022-05-19 22:47:57 +0200

    last commit needed more test's

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,5 @@
 package com.company;
-
 import java.util.*;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,66 +12,35 @@ public class Main {
         String dzialanie = scan.nextLine();
 
         //znak specjanly
-        //Pattern znak = Pattern.compile("([\\+\\-\\/\\*])");
-        //Matcher znak_dzialania = znak.matcher(dzialanie);
+        Pattern znak = Pattern.compile("([\\+\\-\\/\\*])");
+        Matcher znak_dzialania = znak.matcher(dzialanie);
+        //dzielenie ciagu na kilka podznakow
+        String[] zn1 = dzialanie.split("[\\+\\-\\/\\*]");
 
-        //pierwsza liczba
-        //Pattern pierwsza_liczba = Pattern.compile("^(\\d+[\\+\\-\\*\\/]{1})");
-        //Matcher pierwsza  = pierwsza_liczba.matcher(dzialanie);
-
-        //druga liczba
-        //Pattern druga_liczba = Pattern.compile("[0-9]+$");
-        //Matcher druga = druga_liczba.matcher(dzialanie);
-
-        //debug stuff
-        String[] zn1 = dzialanie.split("([\\+\\-\\/\\*])");
-
-        try{
-            int znak1 = Integer.parseInt(String.valueOf(dzialanie.split("(\\d+[\\+\\-\\*\\/]{1})")));
-            System.out.println("pierwsza liczba" + znak1);
-        }catch(NumberFormatException ex){
-            ex.printStackTrace();
-        }
-
-        try{
-            int znak2 = Integer.parseInt(String.valueOf(dzialanie.split("[0-9]+$")));
-            System.out.println("Druga liczba " + znak2);
-        }catch(NumberFormatException ex){
-            ex.printStackTrace();
-        }
-
-//        System.out.println("znak dzialania " + zn1[0]);
-//        System.out.println("pierwsza liczba " + znak1);
-//        System.out.println("druga liczba " + znak2);
-
-
+        //liczby zapisane w longu poniewaz moga miec dlugosc poand 2 miliardow1
+        long znak1 = Long.parseLong(zn1[0]);
+        long znak2 = Long.parseLong(zn1[1]);
 
 
         //jezeli nie wprowadzono prawidlowego znaku
         //ta czesc kodu przerywa wykonanie
-//        if(!znak_dzialania.find()){
-//            System.out.println("Nie znaleziono znaku dzialania");
-//            return;
-//        }
-        String temp = zn1[0];
 
-//        switch (temp){
-//            case "+":
-//                    System.out.println( znak1 + znak2);
-//                break;
-//            case "-":
-//                System.out.println( znak1 - znak2);
-//                break;
-//            case "*":
-//                System.out.println( znak1 * znak2);
-//                break;
-//            case "/":
-//                System.out.println( znak1 / znak2);
-//                break;
-//            default:
-//                System.out.println("Cos poszlo nie tak");
-//                break;
-//        }
+        if(!znak_dzialania.find()){
+            System.out.println("Nie znaleziono znaku dzialania");
+            return;
+        }
+
+        if(dzialanie.contains("+")){
+            System.out.println( znak1 + znak2);
+        } else if(dzialanie.contains("-")){
+            System.out.println( znak1 - znak2);
+        } else if(dzialanie.contains("*")){
+            System.out.println(znak1 * znak2);
+        } else if(dzialanie.contains("/")){
+            System.out.println(znak1 /znak2);
+        } else {
+            System.out.println("Cos poszlo nie tak");
+        }
 
         scan.close();
     }
